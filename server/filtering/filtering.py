adc8b4e7bcf51993001f9deaa02044922d716962
root <root@wbill.cse356.compas.cs.stonybrook.edu>
2024-11-11 07:30:31 +0000

    fixing filtering

--- server/filtering/filtering.py
+++ server/filtering/filtering.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import sessionmaker, session
 from schemas import User, Video, Like
 import numpy as np
 import math
+import sys
 
 engine = create_engine('sqlite:///../../main.db')
 
@@ -22,6 +23,7 @@ def get_latest_data():
     matrix = np.zeros((len(users), len(videos)))
 
     for like in likes:
+        print(like.like_value, like.user_id, like.video_id)
         user_idx = user_ids[like.user_id]
         video_idx = video_ids[like.video_id]
         # Assign 1 for like, -1 for dislike
@@ -29,41 +31,76 @@ def get_latest_data():
 
     return matrix, user_ids, video_ids
 
-def compute_similarity(matrix):
+def compute_similarity(matrix, user_vector):
     
-    # normalize vectors in matrix
-    norm_matrix = matrix / (np.linalg.norm(matrix, axis=1, keepdims=True) + 1e-9)
+    # normalize vectors in matrix: self divide by norm
+    norm_user_vector = np.linalg.norm(user_vector, keepdims=True)
+    # print(np.shape(norm_user_vector))
+    # for 
+    norm_matrix = (np.linalg.norm(matrix, axis=1, keepdims=True))
+    # print(np.shape(norm_matrix))
+    print(np.shape(matrix))
+    print(np.shape(user_vector))
+    # print(norm_matrix)
+    # (1, users)
+    similarity = np.zeros(np.shape(norm_matrix)[1])
 
+    for i, norm_row in enumerate(norm_matrix):
+        if norm_user_vector * norm_row != 0:
+            # print(np.shape(matrix.T[i]), np.shape(user_vector), np.shape(norm_row), np.shape(norm_user_vector))
+            print(user_vector.shape)
+            print(matrix[i].shape)
+            similarity[i] += user_vector.dot(matrix[i]) # ????
+            similarity[i] /= (norm_user_vector * norm_row)
+
+    # np.set_printoptions(threshold=sys.maxsize)
+    # print(matrix)
+    # print(norm_user_vector)
+    
     # compute similarity
-    similarity = np.dot(norm_matrix, norm_matrix.T)
+
+    # similarity = np.sum(similarity.sum(), axis=0)
+    print(similarity)
+    print(np.shape(similarity))
 
     return similarity
 
 def recommend_videos(user_id, num_vids):
     
+    # mappings of user ids and video ids
     matrix, user_ids, video_ids = get_latest_data()
-    
-    sim_matrix = compute_similarity(matrix)
-    # get mapping of user id
     user_map_id = user_ids[user_id]
+    user_vector = matrix[user_map_id]
+    sim_to_others = compute_similarity(matrix, user_vector)
+    # print(sim_to_others)
+    # get mapping of user id
    
-    sim_users = sim_matrix[user_map_id]
-    sim_users[user_map_id] = 0 
+    # sim_users = sim_matrix[user_map_id]
 
-    weighted_scores = np.dot(sim_users, matrix)
+    # removes self to not compare with self
+    # sim_to_others[user_map_id] = 0 
+    # # print(sim_to_others)
+    # print(np.shape(sim_to_others), np.shape(matrix))
 
-    user_interactions = matrix[user_map_id]
-    unviewed_video_ids = np.where(user_interactions == 0)[0]
+    # # similarity scores
+    # weighted_scores = sim_to_others @ matrix
+    # # np.set_printoptions(threshold=sys.maxsize)
+    # # print(weighted_scores)
+    # user_interactions = matrix[user_map_id]
+    # # videos that were not liked/disliked
+    # unviewed_video_ids = np.where(user_interactions == 0)[0]
 
-    # make sure that the view the recommendations have not been viewed yet
-    # prefer:
-    # 1. Recommended videos that have not been viewed
-    # 2. random videos (not been viewed before)
-    # 3. just random videos if eveything has been viewed
-    print(len(unviewed_video_ids))
+    # # make sure that the view the recommendations have not been viewed yet
+    # # prefer:
+    # # 1. Recommended videos that have not been viewed
+    # # 2. random videos (not been viewed before)
+    # # 3. just random videos if eveything has been viewed
 
-    recommended_idxs = unviewed_video_ids[np.argsort(-weighted_scores[unviewed_video_ids])]
-    video_ids_reverse = {v: k for k, v in video_ids.items()}
-    return [video_ids_reverse[idx] for idx in recommended_idxs[:num_vids]]
+    # # print(len(unviewed_video_ids))
+    # # print(weighted_scores[unviewed_video_ids])
+    # recommended_idxs = unviewed_video_ids[np.argsort(-weighted_scores[unviewed_video_ids])]
+    # # print(recommended_idxs)
+    # video_ids_reverse = {v: k for k, v in video_ids.items()}
+    # return [video_ids_reverse[idx] for idx in recommended_idxs[:num_vids]]
 
 print(recommend_videos('019317fc-f35a-7aad-bf97-d2dfbcb272fd', num_vids=5))
